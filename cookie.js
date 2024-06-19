afbfeb2e4cb551d106040140a80c14396908750f
Lucas Martinez <lucas.martinez@ynov.com>
2024-06-19 11:25:16 +0200

    ajout des cookies avec expiration au bout de 24 heures

---
+++ cookie.js
@@ -0,0 +1,7 @@
+let date = new Date(Date.now() + 86400000); //86400000 représente les ms d'une journée (86400 secondes x 1000)
+date = date.toUTCString();
+
+
+document.cookie= 'user= User; path=/; expires=' + date;
+
+// https://www.pierre-giraud.com/javascript-apprendre-coder-cours/cookies/
