fbfa1457e290409d5cb153dfa47165738d466444
caloic <loic.cano@ynov.com>
2024-06-21 10:03:23 +0200

    Link db, Create post system

--- cookie.js
+++
@@ -1,7 +0,0 @@
-let date = new Date(Date.now() + 86400000); //86400000 représente les ms d'une journée (86400 secondes x 1000)
-date = date.toUTCString();
-
-
-document.cookie= 'user= .user; path=/; expires=' + date;
-
-// https://www.pierre-giraud.com/javascript-apprendre-coder-cours/cookies/
