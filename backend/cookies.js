36311fa3fa0c9fa6f05a7c19f00ca5285597c326
Lucas Martinez <lucas.martinez@ynov.com>
2024-06-25 10:33:59 +0200

    like and dislike buttons

---
+++ backend/cookies.js
@@ -0,0 +1,8 @@
+var getActive = browser.tabs.query({ active: true, currentWindow: true });
+getActive.then(setCookie);
+
+function setCookie(tabs) {
+  browser.cookies.set({
+    url: tabs[0].url
+  });
+}
--- frontend/public/main.js
+++ frontend/public/main.js
@@ -58,3 +58,56 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initial fetch des posts pour les afficher lors du chargement de la page
     fetchPosts();
 });
+
+let isLiked = false;
+let isDisliked = false;
+
+const toggleLike = () => {
+    const likeBtn = document.getElementById("likeBTN");
+    const likeCount = document.getElementById("likeCount");
+
+    if(isLiked) {
+        isLiked = false;
+        likeCount.textContent = parseInt(likeCount.textContent) - 1;
+        likeBtn.classList.remove("fa-solid");
+        likeBtn.classList.add("fa-regular");
+    } else {
+        isLiked = true;
+        if (isDisliked) {
+            isDisliked = false;
+            const dislikeCount = document.getElementById("dislikeCount");
+            dislikeCount.textContent = parseInt(dislikeCount.textContext) - 1;
+            const dislikeBtn = document.getElementById("dislikeBtn)");
+            dislikeBtn.classList.remove("fa-solid");
+            dislikeBtn.classicList.add("fa-regular");
+        }
+        likeCount.textContent = parseInt(likeCount.textContent) + 1;
+        likeBtn.classList.remove("fa-regular");
+        likeBtn.classList.add("fa-solid");
+    }
+}
+
+const toggledisLike = () => {
+    const dislikeBtn = document.getElementById("dislikeBTN");
+    const dislikeCount = document.getElementById("dislikeCount");
+
+    if(isDisliked) {
+        isDisliked = false;
+        dislikeCount.textContent = parseInt(dislikeCount.textContent) - 1;
+        dislikeBtn.classList.remove("fa-solid");
+        dislikeBtn.classList.add("fa-regular");
+    } else {
+        isDisliked = true;
+        if (isLiked) {
+            isLiked = false;
+            const likeCount = document.getElementById("likeCount");
+            likeCount.textContent = parseInt(dislikeCount.textContext) - 1;
+            const dislikeBtn = document.getElementById("likeBtn)");
+            likeBtn.classList.remove("fa-solid");
+            likeBtn.classicList.add("fa-regular");
+        }
+        dislikeCount.textContent = parseInt(likeCount.textContent) + 1;
+        dislikeBtn.classList.remove("fa-regular");
+        dislikeBtn.classList.add("fa-solid");
+    }
+}
